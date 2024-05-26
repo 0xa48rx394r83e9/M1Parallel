92062dc3800e35407306fb70a252024528cc9c13
Joseph Paulmer <Joseph.paulmer24@stu.dulwich.org>
2024-05-26 19:18:27 +0800

    Added mpi_wrapper.c ~ source file

--- src/distributed_memory/mpi_wrapper.c
+++ src/distributed_memory/mpi_wrapper.c
@@ -1,13 +1,35 @@
-#ifndef MPI_WRAPPER_H
-#define MPI_WRAPPER_H
+#include "mpi_wrapper.h"
 
-#include <mpi.h>
+void mpi_init()
+{
+    MPI_Init(NULL, NULL);
+}
 
-void mpi_init();
-void mpi_finalize();
-int mpi_rank();
-int mpi_size();
-void mpi_send(void *data, int count, MPI_Datatype datatype, int dest, int tag);
-void mpi_recv(void *data, int count, MPI_Datatype datatype, int source, int tag);
+void mpi_finalize()
+{
+    MPI_Finalize();
+}
 
-#endif
+int mpi_rank()
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    return rank;
+}
+
+int mpi_size()
+{
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    return size;
+}
+
+void mpi_send(void *data, int count, MPI_Datatype datatype, int dest, int tag)
+{
+    MPI_Send(data, count, datatype, dest, tag, MPI_COMM_WORLD);
+}
+
+void mpi_recv(void *data, int count, MPI_Datatype datatype, int source, int tag)
+{
+    MPI_Recv(data, count, datatype, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+}
