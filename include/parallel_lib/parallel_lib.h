5002d2b6046b2e2b3b8cf7dcd38526bb10bede63
Joseph Paulmer <Joseph.paulmer24@stu.dulwich.org>
2024-05-26 19:30:14 +0800

    Forgot to git commit the main header file

---
+++ include/parallel_lib/parallel_lib.h
@@ -0,0 +1,11 @@
+#ifndef PARALLEL_LIB_H
+#define PARALLEL_LIB_H
+
+#include "shared_memory/parallel_for.h"
+#include "shared_memory/thread_pool.h"
+#include "distributed_memory/mpi_wrapper.h"
+#include "distributed_memory/message_passing.h"
+#include "task_parallelism/task.h"
+#include "task_parallelism/task_scheduler.h"
+
+#endif
